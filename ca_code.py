08312272b53a827daa4380d4874aa200c36c692a
Jian <jianyuan@gmail.com>
2014-05-05 04:55:49 +0100

    Add comments

--- ca_code.py
+++ ca_code.py
@@ -14,44 +14,57 @@ PHASE_TAPS = np.array([
   [2,3]   # PRN 10
 ]) - 1 # Minus 1 to take into account Python's zero-indexed arrays
 
+# G1 = 1 + x3 + x10
+POLYNOMIAL_1 = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 1])
+
+# G2 = 1 + x2 + x3 + x6 + x8 + x9 + x10
+POLYNOMIAL_2 = np.array([0, 1, 1, 0, 0, 1, 0, 1, 1, 1])
+
+# Shift register size
+SR_SIZE = 10
+
 def get_phase_taps(prn):
   return PHASE_TAPS[prn-1]
 
 def generate(prn):
-  # G1 = 1 + x3 + x10
-  poly1 = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 1])
-
-  # G2 = 1 + x2 + x3 + x6 + x8 + x9 + x10
-  poly2 = np.array([0, 1, 1, 0, 0, 1, 0, 1, 1, 1])
-
   # Allocate G1 (shift register)
-  sr1 = np.ones(poly1.size)
+  sr1 = np.ones(POLYNOMIAL_1.shape)
 
   # Allocate G2 (shift register)
-  sr2 = np.ones(poly2.size)
+  sr2 = np.ones(POLYNOMIAL_2.shape)
 
   # Get phase tap indices for selected PRN
   phase_taps = get_phase_taps(prn=prn)
 
-  # G3 (sr1) length is ((Size of 2^G1) - 1)
-  L = (2 ** sr1.size) - 1
+  # Maximum-length shift register (MLSR)
+  # Length is ((Length of 2^SR_SIZE) - 1) = 1023
+  L = (2 ** SR_SIZE) - 1
 
   # Allocate G3 (output)
-  G  = np.zeros(L)
+  G = np.zeros(L)
 
   # Generate C/A code sequence
   for i in range(L):
-    tapped = np.mod(np.sum(sr2[phase_taps]), 2)
+    # Take values from sr2 using the index specified by the PRN
+    phase_tapped = np.mod(np.sum(sr2[phase_taps]), 2)
 
-    G[i] = np.mod(sr1[-1] + tapped, 2);
+    # Calculates the current C/A code sequence
+    G[i] = np.mod(sr1[-1] + phase_tapped, 2);
 
-    sr1[-1] = np.mod(np.sum(sr1 * poly1), 2)
+    # Calculates last value in shift register sr1
+    # BEFORE rotating to the right
+    sr1[-1] = np.mod(np.sum(sr1 * POLYNOMIAL_1), 2)
+    # Once rotated, the last value is now the first value
     sr1 = np.roll(sr1, 1)
 
-    sr2[-1] = np.mod(np.sum(sr2 * poly2), 2)
+    # Do the same for shift register sr2
+    sr2[-1] = np.mod(np.sum(sr2 * POLYNOMIAL_2), 2)
     sr2 = np.roll(sr2, 1)
 
   return G
 
+def test_correctness():
+  pass
+
 if __name__ == '__main__':
-  print generate(prn=1)[0:10]
+  print generate(prn=1)
