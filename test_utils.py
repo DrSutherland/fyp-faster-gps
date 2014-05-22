fd1f78271c848118ad6251fad4b323001dfe07d3
Jian Yuan <jianyuan@gmail.com>
2014-05-22 02:26:21 +0100

    Implement modular inversion

--- test_utils.py
+++ test_utils.py
@@ -9,3 +9,8 @@ def test_gcd_correctness():
     assert_equal(utils.gcd(976, 1024), 16)
     assert_equal(utils.gcd(125, 1024), 1)
     assert_equal(utils.gcd(380, 1024), 4)
+
+def test_mod_inverse_correctness():
+    assert_equal(utils.mod_inverse(a=3, n=37), 25)
+    assert_equal(utils.mod_inverse(a=7, n=26), 15)
+    assert_equal(utils.mod_inverse(a=1, n=10), 1)
--- utils.py
+++ utils.py
@@ -1,7 +1,58 @@
-__author__ = 'jyl111'
+from __future__ import division
 
 import fractions
 
 import numpy as np
 
+
+__author__ = 'jyl111'
+
+
 gcd = np.frompyfunc(fractions.gcd, 2, 1)
+
+
+def extended_gcd(a, b):
+    """
+    Extended GCD using Extended Euclidean algorithm
+    References:
+        http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+        https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm
+    """
+    if a == 0:
+        return b, 0, 1
+    else:
+        g, y, x = extended_gcd(b % a, a)
+        return g, x - (b // a) * y, y
+
+
+def mod_inverse(a, n):
+    """
+    Modular inversion using the extended Euclidean algorithm
+    References:
+        http://rosettacode.org/wiki/Modular_inverse#Python
+        https://en.wikibooks.org/wiki/Algorithm_Implementation/Mathematics/Extended_Euclidean_algorithm#Modular_inverse
+    """
+    gcd, x, y = extended_gcd(a, n)
+    if gcd != 1:
+        return None # modular inverse does not exist
+    else:
+        return x % n
+
+    # t = 0
+    # new_t = 1
+    # r = n
+    # new_r = a
+    #
+    # while new_r > 0:
+    #     quotient = r % new_r
+    #     t, new_t = new_t, (t - quotient * new_t)
+    #     r, new_r = new_r, (r - quotient * new_r)
+    #     print quotient, t, r
+    #
+    # if r > 1:
+    #     raise ValueError('a is not invertible')
+    #
+    # if t < 0:
+    #     t += n
+    #
+    # return t
