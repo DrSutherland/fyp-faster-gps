6b6b1e9af560c046c253f5b25a058f186ae3c18e
jianyuan <jianyuan@gmail.com>
2014-06-05 08:41:35 +0100

    Print debug messages

--- sfft_cross_correlation.py
+++ sfft_cross_correlation.py
@@ -22,28 +22,29 @@ def main():
     print multiplied.size
 
     params = Parameters(
-        n=multiplied.size,
+        n=multiplied.size/2,
         k=1,
         B_k_location=2,
         B_k_estimation=2,
         estimation_loops=8,
         location_loops=5,
-        loop_threshold=2,
+        loop_threshold=4,
         tolerance_location=1e-8,
         tolerance_estimation=1e-8
     )
     result = sfft_inverse.execute(params=params, x=multiplied)
-
-    # result = fourier_transforms.ifft(multiplied)
-
     result = fourier_transforms.fftshift(result)
+
+    result_actual = fourier_transforms.ifft(multiplied)
+    result_actual = fourier_transforms.fftshift(result_actual)
+
+    print 'sfft size', result.size
+    print 'original size', result_actual.size
 
     fig = plt.figure()
     ax = fig.gca()
-    ax.plot(
-        result.real,
-        result.imag
-    )
+    ax.plot(np.linspace(-1, 1, result.size), np.abs(result))
+    ax.plot(np.linspace(-1, 1, result_actual.size), result_actual)
     plt.show()
 
 
