a9401196ea0e92e3ab7bbbc9352f380cf4d46397
jianyuan <jianyuan@gmail.com>
2014-06-13 23:05:45 +0100

    Update aliasing demo code

--- sfft_aliasing.py
+++ sfft_aliasing.py
@@ -2,6 +2,7 @@ __author__ = 'jyl111'
 
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.fft import fft, fftshift
 
 
 def execute(a, q):
@@ -30,35 +31,41 @@ def execute(a, q):
     assert n % q == 0
 
     # Calculate number of buckets
-    n_aliased = n/q
+    B = n/q
 
     # Allocate output array
-    b = np.zeros(n_aliased)
+    b = np.zeros(B)
 
     # Hash each input sample into the correct output bucket
     for i in xrange(n):
-        b[i % n_aliased] += a[i]
+        b[i % B] += a[i]
 
     return b
 
 
 def main():
     # Generate a step sequence and then leveling off
-    input_t = np.append(
-        np.linspace(0, 1, 6),
-        np.ones(4)
-    )
+    # input_t = np.append(
+    #     np.linspace(0, 1, 6),
+    #     np.ones(4)
+    # )
+    freq_samp = 1000
+
+    freq = 100
+    t = np.arange(100, dtype=float)/freq_samp
+
+    input_t = np.sin(2*np.pi*freq*t)
 
     # Perform aliasing
     output_t = execute(input_t, q=2)
 
     # Calculate FFTs
-    input_f = np.fft.fft(input_t)
-    output_f = np.fft.fft(output_t)
+    input_f = fft(input_t)
+    output_f = fft(output_t)
 
     # FFT shift
-    input_f = np.fft.fftshift(input_f)
-    output_f = np.fft.fftshift(output_f)
+    input_f = fftshift(input_f)
+    output_f = fftshift(output_f)
 
     # Absolute FFT output
     input_f = np.abs(input_f)
@@ -68,38 +75,29 @@ def main():
 
     plt.subplot(221)
     plt.plot(input_t, '-ob')
-    for i, value in enumerate(input_t):
-        plt.plot([i, i], [value, 0], '--b')
     plt.title('Original Input')
     plt.ylabel('Amplitude')
     plt.xlabel('Sample index')
 
     plt.subplot(222)
     plt.plot(output_t, '-ob')
-    for i, value in enumerate(output_t):
-        plt.plot([i, i], [value, 0], '--b')
     plt.title('Aliased Input, $q=2$')
     plt.ylabel('Amplitude')
     plt.xlabel('Sample index')
-    plt.ylim(ymin=0)
-    plt.xticks(np.arange(output_t.size))
 
     plt.subplot(223)
     plt.plot(input_f, '-ob')
-    for i, value in enumerate(input_f):
-        plt.plot([i, i], [value, 0], '--b')
     plt.title('FFT of Original Input')
     plt.ylabel('Magnitude')
     plt.xlabel('Frequency index')
 
     plt.subplot(224)
     plt.plot(output_f, '-ob')
-    for i, value in enumerate(output_f):
-        plt.plot([i, i], [value, 0], '--b')
     plt.title('FFT of Aliased Input, $q=2$')
     plt.ylabel('Magnitude')
     plt.xlabel('Frequency index')
 
+    plt.tight_layout()
     plt.show()
 
 
