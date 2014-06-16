45af6024b993484060fb4e9a7aaf91209e495001
jianyuan <jianyuan@gmail.com>
2014-06-16 09:48:49 +0100

    Refactor

--- gps_acquisition.py
+++ gps_acquisition.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 __author__ = 'jyl111'
 
 import os
@@ -5,75 +7,94 @@ import matplotlib
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
+from numpy.fft import fft, ifft
 from scipy import signal
 
 import ca_code
 
+
 def acquisition(x, settings, plot_graphs=False):
     # Calculate number of samples per spreading code (corresponding to 1ms of data)
-    samples_per_code = int(round(settings['sampling_frequency'] * settings['code_length'] / float(settings['code_frequency'])))
-    print 'There are %d samples per code' % samples_per_code
+    samples_per_code = int(round(settings['sampling_frequency'] * settings['code_length'] / settings['code_frequency']))
+    print 'samples_per_code = %s' % repr(samples_per_code)
 
+    # Two consecutive 2ms reading
     x_1 = x[0:samples_per_code]
     x_2 = x[samples_per_code:2*samples_per_code]
+    print 'x_1.shape = %s' % repr(x_1.shape)
+    print 'x_2.shape = %s' % repr(x_2.shape)
 
-    x_0dc = x - np.mean(x)
+    # x_0dc = x - np.mean(x)
 
     # Calculate sampling period
     sampling_period = 1.0 / settings['sampling_frequency']
+    print 'sampling_period = %s' % repr(sampling_period)
 
+    # Generate phase points of the local carrier
     phases = np.arange(0, samples_per_code) * 2 * np.pi * sampling_period
+    print 'phases(%s) = %s' % (repr(phases.shape), repr(phases))
 
-    # Calculate number of requency bins
+    # Calculate number of frequency bins depending on search frequency band and frequency step
     assert settings['acquisition_search_frequency_band'] % settings['acquisition_search_frequency_step'] == 0, \
         'acquisition_search_frequency_band should be divisible by acquisition_search_frequency_step'
-    n_frequency_bins = settings['acquisition_search_frequency_band'] / settings['acquisition_search_frequency_step']
-    print 'There are %d frequency bins' % n_frequency_bins
+    n_frequency_bins = int(settings['acquisition_search_frequency_band'] / settings['acquisition_search_frequency_step']) + 1
+    print 'n_frequency_bins = %s' % repr(n_frequency_bins)
 
     # todo generate ca table
 
-    all_results = np.zeros(shape=(n_frequency_bins, samples_per_code))
+    # Allocate memory for the 2D search
+    all_results = np.empty(shape=(n_frequency_bins, samples_per_code))
 
     # Generate all frequency bins
-    frequency_bins = settings['intermediate_frequency'] - \
+    frequency_bins = (
+        settings['intermediate_frequency'] - \
         (settings['acquisition_search_frequency_band'] / 2) + \
         settings['acquisition_search_frequency_step'] * np.arange(n_frequency_bins)
+    ).astype(int)
+    print 'frequency_bins(%s) = %s' % (repr(frequency_bins.shape), repr(frequency_bins))
 
-    # Allocate output dictionary
+    # Allocate memory for output dictionary
     output = {
-        'frequency_shifts': np.zeros(settings['satellites_to_search'].size),
-        'code_shifts': np.zeros(settings['satellites_to_search'].size),
-        'peak_ratios': np.zeros(settings['satellites_to_search'].size),
-        'found': np.zeros(settings['satellites_to_search'].size, dtype=np.bool),
+        'frequency_shifts': np.zeros(settings['satellites_total']),
+        'code_shifts': np.zeros(settings['satellites_total']),
+        'peak_ratios': np.zeros(settings['satellites_total']),
+        'found': np.zeros(settings['satellites_total'], dtype=np.bool),
     }
 
-    for idx, prn in enumerate(settings['satellites_to_search']):
-        print 'Acquiring satellite PRN = %d' % prn
+    for prn in settings['satellites_to_search']:
+        print '* searching PRN = %s' % (repr(prn),)
 
-        ca_code_t = ca_code.generate(prn=prn).repeat(samples_per_code/1023)
-        ca_code_f = np.conj(np.fft.fft(ca_code_t))
+        # todo Move this somewhere else
+        ca_code_time = ca_code.generate(prn=prn).repeat(samples_per_code/1023)
+        ca_code_freq = np.conjugate(fft(ca_code_time))
 
         # Scan Doppler frequencies
-        for freq_bin_i in range(n_frequency_bins):
+        for freq_bin_i in xrange(n_frequency_bins):
             # Generate local sine and cosine carriers
             carrier_sin = np.sin(frequency_bins[freq_bin_i] * phases)
             carrier_cos = np.cos(frequency_bins[freq_bin_i] * phases)
 
             # Demodulation
-            IQ_1 = carrier_sin*x_1 + 1j*carrier_cos*x_1
-            IQ_2 = carrier_sin*x_2 + 1j*carrier_cos*x_2
+            I1 = carrier_sin * x_1
+            Q1 = carrier_cos * x_1
+            I2 = carrier_sin * x_2
+            Q2 = carrier_cos * x_2
 
-            # Baseband signal to frequency domain
-            IQ_1_freq = np.fft.fft(IQ_1)
-            IQ_2_freq = np.fft.fft(IQ_2)
+            # Reconstruct baseband signal
+            IQ1 = I1 + 1j*Q1
+            IQ2 = I2 + 1j*Q2
 
-            # Convolve
-            conv_code_IQ_1 = IQ_1_freq * ca_code_f
-            conv_code_IQ_2 = IQ_2_freq * ca_code_f
+            # Convert to frequency domain
+            IQ1_freq = fft(IQ1)
+            IQ2_freq = fft(IQ2)
+
+            # Multiplication in the frequency domain corresponds to convolution in the time domain
+            conv_code_IQ1 = IQ1_freq * ca_code_freq
+            conv_code_IQ2 = IQ2_freq * ca_code_freq
 
             # IFFT to obtain correlation
-            corr_result_1 = np.abs(np.fft.ifft(conv_code_IQ_1)) ** 2
-            corr_result_2 = np.abs(np.fft.ifft(conv_code_IQ_2)) ** 2
+            corr_result_1 = np.abs(ifft(conv_code_IQ1)) ** 2
+            corr_result_2 = np.abs(ifft(conv_code_IQ2)) ** 2
 
             assert all_results[freq_bin_i, :].shape == corr_result_1.shape == corr_result_2.shape
 
@@ -146,13 +167,13 @@ def acquisition(x, settings, plot_graphs=False):
 
         peak_ratio = peak_value / float(second_peak_value)
         print 'peak_ratio = %f' % peak_ratio
-        output['peak_ratios'][idx] = peak_ratio
+        output['peak_ratios'][prn-1] = peak_ratio
 
         if peak_ratio > settings['acquisition_threshold']:
-            output['found'][idx] = True
+            output['found'][prn-1] = True
             print '* FOUND'
         else:
-            output['found'][idx] = False
+            output['found'][prn-1] = False
             print '* NOT FOUND'
 
     return output
@@ -169,7 +190,8 @@ if __name__ == '__main__':
         'sampling_frequency': 16367600,
         'code_frequency': 1023000,
         'code_length': 1023,
-        'satellites_to_search': np.arange(1, 32+1),
+        'satellites_total': 32,
+        'satellites_to_search': np.arange(15, 23+1),
         'acquisition_search_frequency_band': 14000,
         'acquisition_search_frequency_step': 500,
         'acquisition_threshold': 2.5,
